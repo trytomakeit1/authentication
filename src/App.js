73573e8017c5ad2fca2a71250b9847580e730978
trytomakeit1 <trytomakeit1@gmail.com>
2020-08-06 19:05:30 +0430

    Added routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
 import Signup from './components/signup';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props){
@@ -29,17 +30,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          { 
-            this.state.currentPage === "signup" ? <Signup /> : 
-            
-            <div>
-              <p style={{fontWeight: "600"}}>Please login using your username and password</p>
-              <div className="form-container">
-                <Login />
-                <p style={{fontSize: "1rem"}}>If you don't have an account yet, <a id="aaa" href="#" onClick={()=>{this.redirectTo("signup")}} >sign up</a></p>
-              </div>
-            </div>
-          }
+          
+            <Route exact path={["/login","/"]} render={()=>{
+              return (
+                <div>
+                  <p style={{fontWeight: "600"}}>Please login using your username and password</p>
+                  <div className="form-container">
+                      <Login />
+                      <p style={{fontSize: "1rem"}}>If you don't have an account yet, <Link id="singup" to="signup" onClick={()=>{this.redirectTo("signup")}} >sign up</Link></p>
+                  </div>
+                </div>
+              )
+            }} />
+            <Route path="/signup" component={Signup} />
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
+
 
 //<React.StrictMode>
 ReactDOM.render(
   
-    <App page="login" />
+    <BrowserRouter><App page="login" /></BrowserRouter>
   ,
   document.getElementById('root')
 );
